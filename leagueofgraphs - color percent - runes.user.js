a465b849d88997b865f5ac1a68bd74a78913a6ab
Coyote-31 <coyotepouil@gmail.com>
2021-12-31 07:38:46 +0100

    🎉 Update color percent rune to 1.0
    All basics features are implemented.

--- leagueofgraphs - color percent - runes.user.js
+++ leagueofgraphs - color percent - runes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         leagueofgraphs - color percent - runes
 // @namespace    https://www.leagueofgraphs.com/
-// @version      0.1.1
+// @version      1.0.0
 // @description  Color percentage on runes page.
 // @author       Coyote
 // @license MIT
@@ -22,18 +22,18 @@ const lowPopLimit = 20.0;
 const lowPopColor = "coral"; // coral
 
 const mediumPopLimit = 50.0;
-const mediumPopColor = "lightseagreen"; // lightseagreen - dodgerblue
+const mediumPopColor = "gold"; // gold
 
-const highPopColor = "lightgreen"; // greenyellow - lawngreen - lightgreen
+const highPopColor = "#2AA3CC"; // lightseagreen
 
 // Victory :
 const lowVicLimit = 48.0;
 const lowVicColor = "coral"; // coral
 
 const mediumVicLimit = 50.0;
-const mediumVicColor = "lightseagreen"; // lightseagreen - dodgerblue
+const mediumVicColor = "gold"; // gold
 
-const highVicColor = "lightgreen"; // greenyellow - lawngreen - lightgreen
+const highVicColor = "#2DEB90"; // greenyellow - lawngreen - lightgreen
 
 // Count script loads
 let scriptCount = 0;
@@ -48,8 +48,137 @@ function main () {
     scriptCount++;
     console.log("Script Coyote Loaded #" + scriptCount);
 
+    // Globals
+    let percentsPopGlobal = $("td.globalPopularityCell .progressBarTxt");
+    let percentsVicGlobal = $("td.globalWinrateCell .progressBarTxt");
+
+    colorPop(percentsPopGlobal);
+    colorVic(percentsVicGlobal);
+
+    // Runes tooltips in img
+    let runeImgs = $("table.perksTable td > img");
+    colorPopup(runeImgs);
+
 }
 
-function colorPop () {}
+function colorPop (percents) {
+    $.each(percents, function(i, percent) {
 
-function colorVic () {}
+        let number = percent.innerHTML.replace('%', '');
+
+        if (0.0 <= number && number < lowPopLimit) {
+            percent.style.color = lowPopColor;
+
+        } else if (lowPopLimit <= number && number < mediumPopLimit) {
+            percent.style.color = mediumPopColor;
+            
+        } else if (mediumPopLimit <= number && number < 100.0) {
+            percent.style.color = highPopColor;
+        }
+    });
+}
+
+function colorVic (percents) {
+    
+    $.each(percents, function(i, percent) {
+
+        let number = percent.innerHTML.replace('%', '');
+
+        if (0.0 <= number && number < lowVicLimit) {
+            percent.style.color = lowVicColor;
+
+        } else if (lowVicLimit <= number && number < mediumVicLimit) {
+            percent.style.color = mediumVicColor;
+            
+        } else if (mediumVicLimit <= number && number < 100.0) {
+            percent.style.color = highVicColor;
+        }
+    });
+}
+
+function colorPopup (runeImgs) {
+
+    const regex = /<highlight>\d{1,3}\.\d%<\/highlight>/g;
+
+    $.each(runeImgs, function(i, /** @type {HTMLElement} */ runeImg) {
+
+        let tooltip = runeImg.getAttribute("tooltip");
+
+        let numbers = tooltip.match(regex);
+        
+        if (numbers != null) {
+    
+            // Popularity
+            let popNumPercent = numbers[0].replace("<highlight>", "").replace("</highlight>", "")
+            let popNum = popNumPercent.replace("%", '');
+
+            if (0.0 <= popNum && popNum < lowPopLimit) {
+                // numbers[0] = numbers[0].replace("<highlight>", "<highlight><strong>").replace("</highlight>", "</strong></highlight>");
+                runeImg.setAttribute("tooltip", tooltip.replace(popNumPercent, "<span style=\"color:" + lowPopColor + " !important;\">" + popNumPercent + "</span>"));
+    
+            } else if (lowPopLimit <= popNum && popNum < mediumPopLimit) {
+                // numbers[0] = numbers[0].replace("<highlight>", "<highlight><div>").replace("</highlight>", "</div></highlight>");
+                runeImg.setAttribute("tooltip", tooltip.replace(popNumPercent, "<span style=\"color:" + mediumPopColor + " !important;\">" + popNumPercent + "</span>"));
+                
+            } else if (mediumPopLimit <= popNum && popNum < 100.0) {
+                // numbers[0] = numbers[0].replace("<highlight>", "<highlight><a>").replace("</highlight>", "</a></highlight>");
+                runeImg.setAttribute("tooltip", tooltip.replace(popNumPercent, "<span style=\"color:" + highPopColor + " !important;\">" + popNumPercent + "</span>"));
+            }
+    
+            // refresh tooltip :
+            tooltip = runeImg.getAttribute("tooltip");
+
+            // Victory
+            let vicNumPercent = numbers[1].replace("<highlight>", "").replace("</highlight>", "");
+            let vicNum = vicNumPercent.replace("%", '');
+
+            let vicNumPercentReplaced = "";
+            let isIdenticalNum = numbers[0] === numbers[1];
+            let tooltipParts;
+
+            if (0.0 <= vicNum && vicNum < lowVicLimit) {
+
+                vicNumPercentReplaced = "<span style=\"color:" + lowVicColor + " !important;\">" + vicNumPercent + "</span>";
+
+                if (isIdenticalNum) {
+                    tooltipParts = tooltip.split(vicNumPercent);
+                    tooltipParts[0] = tooltipParts[0] + vicNumPercent;
+                    tooltipParts[1] = tooltipParts[1] + vicNumPercentReplaced;
+                    tooltip = tooltipParts.join("");
+                    runeImg.setAttribute("tooltip", tooltip);
+                } else {
+                    runeImg.setAttribute("tooltip", tooltip.replace(vicNumPercent, vicNumPercentReplaced));
+                }
+    
+            } else if (lowVicLimit <= vicNum && vicNum < mediumVicLimit) {
+
+                vicNumPercentReplaced = "<span style=\"color:" + mediumVicColor + " !important;\">" + vicNumPercent + "</span>";
+
+                if (isIdenticalNum) {
+                    tooltipParts = tooltip.split(vicNumPercent);
+                    tooltipParts[0] = tooltipParts[0] + vicNumPercent;
+                    tooltipParts[1] = tooltipParts[1] + vicNumPercentReplaced;
+                    tooltip = tooltipParts.join("");
+                    runeImg.setAttribute("tooltip", tooltip);
+                } else {
+                    runeImg.setAttribute("tooltip", tooltip.replace(vicNumPercent, vicNumPercentReplaced));
+                }
+                
+            } else if (mediumVicLimit <= vicNum && vicNum < 100.0) {
+
+                vicNumPercentReplaced = "<span style=\"color:" + highVicColor + " !important;\">" + vicNumPercent + "</span>";
+
+                if (isIdenticalNum) {
+                    tooltipParts = tooltip.split(vicNumPercent);
+                    tooltipParts[0] = tooltipParts[0] + vicNumPercent;
+                    tooltipParts[1] = tooltipParts[1] + vicNumPercentReplaced;
+                    tooltip = tooltipParts.join("");
+                    runeImg.setAttribute("tooltip", tooltip);
+                } else {
+                    runeImg.setAttribute("tooltip", tooltip.replace(vicNumPercent, vicNumPercentReplaced));
+                }
+            }
+        }
+
+    });
+}
